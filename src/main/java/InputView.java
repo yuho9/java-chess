3b425191abddabc775f5e2e8c665eb01f8d17e13
yuho9 <146949649+yuho9@users.noreply.github.com>
2024-08-02 20:05:35 +0900

    시작/종료 입력

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 
 public class InputView {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public boolean start() {
+		System.out.println("체스 게임을 시작합니다.");
+		System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+		
+		String start = sc.next();
+		
+		if(start.equals("start"))
+			return true;
+		if(start.equals("end"));
+			return false;				
+	}
 	
 	
 
--- src/main/java/PlayChess.java
+++ src/main/java/PlayChess.java
@@ -3,6 +3,10 @@ public class PlayChess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		InputView Input = new InputView();
+		
+		Input.start();
 
 	}
 
